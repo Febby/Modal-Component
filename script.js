6db469185386d1c2378db0ca4eca43559fc26409
febby <gunawan.febby@gmail.com>
2021-05-27 16:18:40 +0700

    Update script.js
    - Added "escape" key as one of the way to close the overlay.
    
    - Clean & refactor the scripts

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-  'use strict';
+'use strict';
 
-  // Variable for DOM manipulation
+// Variable for DOM manipulation
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
@@ -8,20 +8,32 @@ const btnShowModal = document.querySelectorAll('.show-modal');
 
 
 
-const showModal = function(){
+const showModal = function () {
     console.log("Button clicked");
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 }
 
-const hideModal = function(){
+const hideModal = function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 }
 
 for (let index = 0; index < btnShowModal.length; index++) {
-   btnShowModal[index].addEventListener('click', showModal)
+    btnShowModal[index].addEventListener('click', showModal)
 }
 
 btnCloseModal.addEventListener('click', hideModal);
 overlay.addEventListener('click', hideModal);
+
+document.addEventListener('keydown', function (e) {
+    console.log('e :>> ', e.key);
+
+    // Check if the Escape key was pressed then proceed
+    // & check if the modal does not have the hidden class
+    // if both condition are true then invoke hideModal function
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+
+        hideModal();
+    }
+})
